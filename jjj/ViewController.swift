18ed6460f3cc72c0e2b236948b07290748d99f55
jeanwei <jeanwei10292002@gmail.com>
2019-12-29 16:31:48 +0800

    12/29 服務費 個人費用計算

--- jjj/ViewController.swift
+++ jjj/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //服務費
+    @IBOutlet weak var tep: UITextField!
+    //金額
+    @IBOutlet weak var money: UITextField!
+    //總計
+    @IBOutlet weak var totalLabel: UILabel!
+    //分擔的人數
+    @IBOutlet weak var sharepeople: UITextField!
+    //分擔
+    @IBOutlet weak var shareLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //計算按鈕
+    @IBAction func coo(_ sender: UIButton) {
+        //將金額 服務費轉成 ＤＯＵＢＬＥ，去計算 服務費
+        let tepmu = Double(money.text!)! * Double(tep.text!)! / 100
+        //將 總計算出
+        let total = Double(money.text!)! + tepmu
+        //顯示總計 一人分擔多少
+        totalLabel.text = String(total)
+        shareLabel.text = String(total / Double(sharepeople.text!)!)
+    }
+    //點選空白處鍵盤收起
+       override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+              self.view.endEditing(true)
+          }
 }
 
